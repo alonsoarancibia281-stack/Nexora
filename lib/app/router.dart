e88386e9999ac34aa3a3b330975880ef83aaac4f
alonsoarancibia281-stack <alonsoarancibia281@gmail.com>
2026-08-08 01:30:11 -0500

    phase 1: route password recovery

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import '../features/authentication/presentation/login_screen.dart';
 import '../features/authentication/presentation/register_screen.dart';
+import '../features/authentication/presentation/forgot_password_screen.dart';
 import '../features/email_verification/presentation/verify_email_screen.dart';
 import '../features/onboarding/presentation/home_screen.dart';
 
@@ -9,6 +10,7 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
     GoRoute(path: '/register', builder: (_, __) => const RegisterScreen()),
+    GoRoute(path: '/forgot-password', builder: (_, __) => const ForgotPasswordScreen()),
     GoRoute(
       path: '/verify',
       builder: (_, state) => VerifyEmailScreen(
